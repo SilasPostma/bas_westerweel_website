258928ff924c065ae5db562e4a6f52c402422745
Silas Postma <silas.postma@student.uva.nl>
2025-12-19 00:31:48 +0100

    beschrijving: "COACH | TRAINER | SPREKER",

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Bas Westerweel",
-  description: "COACH | TRAINER | SPEAKER",
+  description: "COACH | TRAINER | SPREKER",
 };
 
 export default function RootLayout({
